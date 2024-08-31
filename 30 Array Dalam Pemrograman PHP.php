bfdddc1345356af360ad82650a269bcb734335f8
Fachri Rizki <fachririzki2087@gmail.com>
2024-08-31 18:46:29 +0700

    Array dalam pemograman

---
+++ 30 Array Dalam Pemrograman PHP.php
@@ -0,0 +1,10 @@
+<?php
+$produk1 = "Modem";
+$produk2 = "Hardisk";
+$produk3 = "Flashdisk";
+
+echo "$produk1<br>";
+echo "$produk2<br>";
+echo "$produk3<br>";
+
+echo "<i><p><strrong>By Fachri Rizki";
