7f8f0111b606194a81d20de84ce49cdda6644750
Fachri Rizki <fachririzki2087@gmail.com>
2024-08-31 18:54:14 +0700

    apa itu array

---
+++ 31 1. Apa itu Array.php
@@ -0,0 +1,7 @@
+
+<?php
+$nama_variabel=array("isi variabel1","isi variabel2","isi variabel3");
+echo "Variabel " . $nama_variabel[0] .",". $nama_variabel[1] .",". $nama_variabel[2] ."!";
+
+
+echo "<i><p><strrong>By Fachri Rizki";
