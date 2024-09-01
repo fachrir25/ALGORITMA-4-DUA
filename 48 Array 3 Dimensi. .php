651027d129e5914b94ead3e2bcafc19f1de8c9cc
Fachri Rizki <fachririzki2087@gmail.com>
2024-09-01 22:06:49 +0700

    array 3

--- 48 Array 3 Dimensi. .php
+++ 48 Array 3 Dimensi. .php
@@ -14,4 +14,5 @@ echo"output array <br>";
 echo $nilai[0] [0] [0]." ".$nilai[0] [0] [1]." ".$nilai[0] [0] [2]."<br>";
 echo $nilai[0] [1] [0]." ".$nilai[0] [1] [1]." ".$nilai[0] [1] [2]."<br>";
 echo $nilai[0] [2] [0]." ".$nilai[0] [2] [1]." ".$nilai[0] [2] [2]."<br>";
+echo "<p><strong> By Fachri Rizki</strong>";
 
