764b3859029a399ac09a849cc8d04191f6341b35
Fachri Rizki <fachririzki2087@gmail.com>
2024-09-01 21:51:14 +0700

    foreach

---
+++ 44 membuat tabel html.php
@@ -0,0 +1,52 @@
+
+
+<table>
+	<tr>
+		<th>No</th>
+		<th>Bulan</th>
+		<th>Penjualan</th>
+	</tr>';
+{
+	<tr>
+		<td>' . $no . '</td>
+		<td>' . $bulan . '</td>
+		<td>' . $nilai . '</td>
+	</tr>';
+	$no++;
+}
+$tabel .= '</table>';
+echo $tabel;
+Hasil:	
+<table>
+	<tr>
+		<th>No</th>
+		<th>Bulan</th>
+		<th>Penjualan</th>
+	</tr>
+	<tr>
+		<td>1</td>
+		<td>Januari</td>
+		<td>5.500</td>
+	</tr>
+	<tr>
+		<td>2</td>
+		<td>Februari</td>
+		<td>7.500</td>
+	</tr>
+	<tr>
+		<td>3</td>
+		<td>Maret</td>
+		<td>11.500</td>
+	</tr>
+	<tr>
+		<td>4</td>
+		<td>April</td>
+		<td>8.800</td>
+	</tr>
+	<tr>
+		<td>5</td>
+		<td>Mei</td>
+		<td>7.500</td>
+	</tr>
+</table>
+
