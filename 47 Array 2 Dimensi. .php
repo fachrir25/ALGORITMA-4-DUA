0f6d1185e025f37fdbd1eb12b7b884dff07932e9
Fachri Rizki <fachririzki2087@gmail.com>
2024-09-01 22:04:21 +0700

    array 2

---
+++ 47 Array 2 Dimensi. .php
@@ -0,0 +1,28 @@
+<?php
+$nilai=array(
+        array(90,65,83),
+        array(90,78,97),
+        array(78,90,78)
+      );
+echo"output array <br>";
+echo $nilai[0] [0]." ".$nilai[0] [1]." ".$nilai[0] [2]."<br>";
+echo $nilai[1] [0]." ".$nilai[1] [1]." ".$nilai[1] [2]."<br>";
+echo $nilai[2] [0]." ".$nilai[2] [1]." ".$nilai[2] [2]."<br>";
+<?php
+$nilai=array(
+        array(
+          array(90,65,83),
+          array(90,78,97),
+          array(78,90,78)
+        ), 
+        array(array(90,65,83),
+          array(90,78,97),
+          array(78,90,78)
+        )
+      );
+echo"output array <br>";
+echo $nilai[0] [0] [0]." ".$nilai[0] [0] [1]." ".$nilai[0] [0] [2]."<br>";
+echo $nilai[0] [1] [0]." ".$nilai[0] [1] [1]." ".$nilai[0] [1] [2]."<br>";
+echo $nilai[0] [2] [0]." ".$nilai[0] [2] [1]." ".$nilai[0] [2] [2]."<br>";
+
+echo "<p><strong> By Fachri Rizki</strong>";
