d05889e6bb7fd643e1457b1d1d2e31f9201ced5c
Fachri Rizki <fachririzki2087@gmail.com>
2024-09-01 23:58:43 +0700

    array push

--- 54 array_push..php
+++ 54 array_push..php
@@ -1,6 +1,6 @@
 <?php
-echo "<pre>"
+echo "<pre>";
 $buah = ["apel", "jeruk"];
 array_push($buah, "pisang", "mangga");
 print_r($buah);
-echo "</pre>"
+echo "<pre>";
---
+++ 57 array_unshift.php
@@ -0,0 +1,6 @@
+<?php
+echo "<pre>";
+$buah = ["apel", "jeruk"];
+array_unshift($buah, "pisang", "mangga");
+print_r($buah);
+echo "<pre>";
