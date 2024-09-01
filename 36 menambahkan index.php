8c1f315afe86a7f1e8f5b75050b536ffbafd590c
Fachri Rizki <fachririzki2087@gmail.com>
2024-09-02 00:25:49 +0700

    menambahkan index

---
+++ 36 menambahkan index.php
@@ -0,0 +1,19 @@
+<?php
+// membuat array
+$user = [
+    "Lisa",
+    "muhar",
+    "fachri"
+];
+
+// mengisi array pada indek ke-1 ("muhar")
+$user[1] = "amellya";
+
+// mencetak isi array
+echo "<pre>";
+print_r($user);
+echo "</pre>";
+
+
+echo "<p><strong>By Fachri</strong>";
+?>
